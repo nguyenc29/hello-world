08b4fb8ac58a0085a7a6f867d1f4d8061f9ca85d
nguyenc29 <46756206+nguyenc29@users.noreply.github.com>
2019-01-16 11:48:33 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/**************************************************
+* Hello
+***************************************************
+* Lab 1 / Introduction to the Course
+*__________________________________________________
+* Carolyn Nguyen
+* January 16, 2019
+* CMSC 255 - 004
+***************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Carolyn Nguyen");
+		System.out.println("Hello");
+		System.out.println("Introduction to the Course");
+		System.out.println("2019");
+		System.out.println("CMSC 255 - 004");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
